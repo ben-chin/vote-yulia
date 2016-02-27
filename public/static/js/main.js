9b7792834f5dbeff48cff5b4e13f530ec7ccf00e
ben-chin <ben.chin101@gmail.com>
2016-02-27 01:30:35 +0000

    Scrolling spy nav working

--- public/static/js/main.js
+++ public/static/js/main.js
@@ -1,1 +1,11 @@
-console.log('hi main');
+$(document).ready(function() {
+
+    $('body').on('activate.bs.scrollspy', function (e) {
+        if ($(e.target).data('topHack')) {
+            $(this).removeClass('is-darkNavbar');
+        } else {
+            $(this).removeClass('is-darkNavbar').addClass('is-darkNavbar');
+        }
+    });
+
+});
