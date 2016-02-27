84b7e80046fe7e99d92181809238f1d6134d2324
ben-chin <ben.chin101@gmail.com>
2016-02-27 01:44:36 +0000

    Color animations and smooth scroll

--- public/static/js/main.js
+++ public/static/js/main.js
@@ -1,5 +1,14 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
+    // Smooth scroll
+    $('a.has-smoothScroll').click(function () {
+        $('html, body').animate({
+            scrollTop: ($($.attr(this, 'href')).offset().top + 1),
+        }, 700);
+        return false;
+    });
+
+    // Scroll spy - toggle nav
     $('body').on('activate.bs.scrollspy', function (e) {
         if ($(e.target).data('topHack')) {
             $(this).removeClass('is-darkNavbar');
